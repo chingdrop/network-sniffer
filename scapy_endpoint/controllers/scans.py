2d17dfd7862d3ac56dc176111c68557873147b1e
Craig Hurley <hurleytheengineer@gmail.com>
2023-05-07 19:04:46 -0400

    move more one liners to scans

--- scapy_endpoint/controllers/scans.py
+++ scapy_endpoint/controllers/scans.py
@@ -17,7 +17,36 @@ class Scans(Controller):
         ],
     )
     def ACKscan(self):
-        ans, unans = sr(IP(dst=self.app.pargs.target_host)/TCP(dport=[80,666],flags="A"))
+        target = self.app.pargs.target_host
+        ans, unans = sr(IP(dst=target)/TCP(dport=[80,666],flags="A"))
         for s,r in ans:
             if s[TCP].dport == r[TCP].sport:
                print(f"Port {s[TCP].dport} is unfiltered.")
+
+    @ex(
+        help='starts an XMAS scan',
+        arguements=[
+            (['target_host'], 
+             {'help': 'target host IP',
+              'action': 'active scan'})
+        ],
+    )
+    def XMASscan(self):
+        target = self.app.pargs.target_host
+        ans, unans = sr(IP(dst=target)/TCP(dport=666,flags="FPU"))
+        for s,r in ans:
+            if s[TCP].dport is not None:
+                 print(f"{s[TCP].dport} is open.")
+
+    @ex(
+        help='starts an IP scan',
+        arguements=[
+            (['target_range'], 
+             {'help': 'target IP range',
+              'action': 'active scan'})
+        ],
+    )
+    def IPscan(self):
+        target = self.app.pargs.target_host
+        ans, unans = sr(IP(dst=target,proto=(0,255))/"SCAPY",retry=2)
+        ans.summary(lambda s,r: r.sprintf("%IP.proto% is listening."))
