cd24f0423c3e9ddc883db2c7b12d07904bdd6f43
Craig Hurley <hurleytheengineer@gmail.com>
2023-05-07 22:45:02 -0400

    sniff controller

---
+++ scapy_endpoint/controllers/sniff.py
@@ -0,0 +1,23 @@
+from cement import Controller, ex
+from scapy import sniff
+
+
+class Sniff(Controller):
+    class Meta:
+        label = 'sniff'
+        stacked_type = 'embedded'
+        stacked_on = 'base'
+
+    @ex(
+        help='starts sniffing on the specified interface.',
+        arguements=[
+            (['wlan_interface'], 
+             {'help': 'wlan interface',
+              'action': 'sniff wlan'})
+        ],
+    )
+    def wlan_sniff(self):
+        interface = self.app.pargs.wlan_interface
+        sniff(iface=interface, \
+        prn=lambda x:x.sprintf( \
+        "{Dot11Beacon:%Dot11.addr3%\t%Dot11Beacon.info%\t%PrismHeader.channel%\t%Dot11Beacon.cap%}"))
