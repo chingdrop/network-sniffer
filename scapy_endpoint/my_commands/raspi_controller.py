82490da7b652909e373fa09e238fa7210bd84940
Craig Hurley <hurleytheengineer@gmail.com>
2023-05-26 12:12:56 -0400

    raspi controller

---
+++ scapy_endpoint/my_commands/raspi_controller.py
@@ -0,0 +1,23 @@
+import socket
+import fcntl
+import struct
+
+class RaspiController:
+
+    def get_local_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+
+        try:
+            # doesn't have to be reachable
+            s.connect(('1.1.1.1', 1))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+
+        return ip
+    
+    def get_local_subnet(self, iface):
+        return socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack(b'256s', iface.encode()))[20:24])
