62b237a43eb151d07cd267d6a90a88ca35636a2f
Craig Hurley <hurleytheengineer@gmail.com>
2023-05-26 14:56:16 -0400

    moved pings

--- scapy_endpoint/controllers/discover.py
+++ scapy_endpoint/controllers/discover.py
@@ -1,11 +1,59 @@
 from cement import Controller, ex
-from scapy.all import sr, ARP, Ether, IP, ICMP
+from scapy.all import sr, srp, ARP, Ether, IP, ICMP, TCP, UDP
 
-from commands.host_discovery import HostDiscovery
-from commands.raspi_controller import RaspiController
 
+class Ping:
+
+    def arp_ping(self, target):
+        host_list = []
+        ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=target), timeout=3, verbose=0)
+        for s,r in ans:
+            host = {
+                    "MAC": r[Ether].dst,
+                    "IP": r[ARP].psrc
+                }
+            host_list.append(host)
+            
+        return host_list
+    
+    def icmp_ping(self, target):
+        host_list = []
+        ans, unans = sr(IP(dst=target)/ICMP(), timeout=3, verbose=0)
+        for s,r in ans:
+            host = {
+                    "MAC": r[Ether].dst,
+                    "IP": r[IP].dst
+                }
+            host_list.append(host)
+            
+        return host_list
+    
+    def tcp_ping(self, target):
+        host_list = []
+        ans, unans = sr(IP(dst=target)/TCP(dport=80,flags="S"), timeout=3, verbose=0)
+        for s,r in ans:
+            host = {
+                    "MAC": r[Ether].dst,
+                    "IP": r[IP].dst
+                }
+            host_list.append(host)
+            
+        return host_list
+    
+    def udp_ping(self, target):
+        host_list = []
+        ans, unans = sr(IP(dst=target)/UDP(dport=0), timeout=3, verbose=0)
+        for s,r in ans:
+            host = {
+                    "MAC": r[Ether].dst,
+                    "IP": r[IP].dst
+                }
+            host_list.append(host)
+            
+        return host_list
 
 class Discover(Controller):
+    
     class Meta:
         label = 'discover'
         stacked_type = 'embedded'
@@ -22,7 +70,7 @@ class Discover(Controller):
     def quick_discover(self):
         iface = self.app.pargs.iface
         network_ip = RaspiController.get_network_ip(iface)
-        arp_list = HostDiscovery.arp_ping(str(network_ip))
+        arp_list = Ping.arp_ping(str(network_ip))
 
     @ex(
         help='starts an ARP ping to discover live hosts',
@@ -35,7 +83,7 @@ class Discover(Controller):
     def full_discover(self):
         iface = self.app.pargs.iface
         network_ip = RaspiController.get_network_ip(iface)
-        arp_list = HostDiscovery.arp_ping(str(network_ip))
-        icmp_list = HostDiscovery.icmp_ping(str(network_ip))
-        tcp_list = HostDiscovery.tcp_ping(str(network_ip))
-        udp_list = HostDiscovery.udp_ping(str(network_ip))
+        arp_list = Ping.arp_ping(str(network_ip))
+        icmp_list = Ping.icmp_ping(str(network_ip))
+        tcp_list = Ping.tcp_ping(str(network_ip))
+        udp_list = Ping.udp_ping(str(network_ip))
