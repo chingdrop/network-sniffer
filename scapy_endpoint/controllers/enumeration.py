2778834d9e92673e5f1d9e33db213c6e75a0ee66
Craig Hurley <hurleytheengineer@gmail.com>
2023-06-07 13:07:17 -0400

    fix quick enum

--- scapy_endpoint/controllers/enumeration.py
+++ scapy_endpoint/controllers/enumeration.py
@@ -32,16 +32,18 @@ class LANEnumeration(Controller):
         for host in live_hosts:
             print(f'\nBeginning port scan for {host["IP"]}!')
 
-            print('\nACK Scan...')
-            ack_port = scans.ack_scan(host["IP"], ports)
-
             print('\nXmas Scan...')
+            print('Determining if host has any open or filtered ports')
             xmas_open, xmas_filtered = scans.xmas_scan(host["IP"], ports)
+
+            print('\nACK Scan...')
+            print('Testing the firewall rules for detected open ports')
+            ack_open = scans.ack_scan(host["IP"], xmas_open)
 
             print('\nProtocol Scan...')
             open_protos = scans.protocol_scan(host["IP"])
 
-            if ack_port or xmas_open or xmas_filtered or open_protos:
+            if ack_open or xmas_open or open_protos:
                 target_list.append(host)
         
         print('\n'.join(f'{target["IP"]} : {target["MAC"]} could be a potential target' for target in target_list))
