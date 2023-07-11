5dc9b28982346c1a39417039535b1e92591e870c
Craig Hurley <hurleytheengineer@gmail.com>
2023-07-10 22:52:36 -0400

    staticmethod

--- scapy_endpoint/controllers/commands/multi_proc.py
+++ scapy_endpoint/controllers/commands/multi_proc.py
@@ -4,20 +4,16 @@ from scapy_endpoint.controllers.commands.enums import NON_PRIVILEGED_LOW_PORT, B
 
 class MultiProcTasks:
 
-    def __init__(self) -> None:
-        self.scans = Scans()
-        self.low_port_range = [i for i in range(1, NON_PRIVILEGED_LOW_PORT)]
-        self.proto_range = [i for i in range(1, BASIC_PROTOCOLS)]
-
-    def mp_scan(
-            self,
-            ip: int,
-            ):
+    @staticmethod
+    def mp_scan(host: int):
+        scans = Scans()
+        low_port_range = [i for i in range(1, NON_PRIVILEGED_LOW_PORT)]
+        proto_range = [i for i in range(1, BASIC_PROTOCOLS)]
+        ack_unfil, _ = scans.ack_scan(host['IP'], low_port_range)
+        xmas_open, _ = scans.xmas_scan(host['IP'], low_port_range)
+        proto_list = scans.protocol_scan(host['IP'], proto_range)
         
-        ack_unfil, _ = self.scans.ack_scan(ip, self.low_port_range)
-        xmas_open, _ = self.scans.xmas_scan(ip, self.low_port_range)
-        proto_list = self.scans.protocol_scan(ip, self.proto_range)
         if ack_unfil or xmas_open or proto_list:
-            return ip
+            return host
         else:
             return None
