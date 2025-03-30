d5bccffe6fcacf9b4c3d40af1fe2eeff441b6ddc
Craig Hurley <chingdorp@pm.me>
2025-03-30 08:07:32 -0400

    fix params being in wrong order

--- src/network_sniffer/resolve.py
+++ src/network_sniffer/resolve.py
@@ -4,19 +4,19 @@ from network_sniffer.packet import BroadcastAdapter, create_dns_pkt
 bca = BroadcastAdapter()
 
 
-def resolve_a_record(target_domain, name_server):
-    pkt = create_dns_pkt(target_domain, target_domain, qtype="A")
+def resolve_a_record(name_server, target_domain):
+    pkt = create_dns_pkt(name_server, target_domain, qtype="A")
     ans = bca.send1(pkt, timeout=3, verbose=0)
     return ans.an.rdata
 
 
-def resolve_soa_record(target_domain, name_server):
-    pkt = create_dns_pkt(target_domain, target_domain, qtype="SOA")
+def resolve_soa_record(name_server, target_domain):
+    pkt = create_dns_pkt(name_server, target_domain, qtype="SOA")
     ans = bca.send1(pkt, timeout=3, verbose=0)
     return ans.an.mname, ans.an.rname
 
 
-def resolve_mx_record(target_domain, name_server):
-    pkt = create_dns_pkt(target_domain, target_domain, qtype="SOA")
+def resolve_mx_record(name_server, target_domain):
+    pkt = create_dns_pkt(name_server, target_domain, qtype="SOA")
     ans = bca.send1(pkt, timeout=3, verbose=0)
     return [x.exchange for x in ans.an.iterpayloads()]
