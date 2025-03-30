bed9f77f0c404fae15748d5d7b97327e851dde12
Craig Hurley <chingdorp@pm.me>
2025-03-30 08:15:01 -0400

    add the option for file logging

--- src/network_sniffer/logger.py
+++ src/network_sniffer/logger.py
@@ -5,7 +5,9 @@ from pathlib import Path
 from network_sniffer.files_and_storage import create_directory, create_file
 
 
-def setup_logger(name: str, level: str = "warning") -> logging.Logger:
+def setup_logger(
+    name: str, level: str = "warning", file: bool = False
+) -> logging.Logger:
 
     log_level = {
         "critical": logging.CRITICAL,
@@ -19,18 +21,19 @@ def setup_logger(name: str, level: str = "warning") -> logging.Logger:
         return logger
     logger.setLevel(log_level[level])
 
-    logs_dir = Path.cwd() / "logs"
-    logs_file = logs_dir / f"{name}_logfile.log"
-    if not logs_dir.exists():
-        create_directory(logs_dir)
-    if not logs_file.exists():
-        create_file(logs_file)
-    file_handler = logging.FileHandler(logs_file)
-    file_handler.setLevel(log_level[level])
+    if file:
+        logs_dir = Path.cwd() / "logs"
+        logs_file = logs_dir / f"{name}_logfile.log"
+        if not logs_dir.exists():
+            create_directory(logs_dir)
+        if not logs_file.exists():
+            create_file(logs_file)
+        file_handler = logging.FileHandler(logs_file)
+        file_handler.setLevel(log_level[level])
+        logger.addHandler(file_handler)
 
     stream_handler = colorlog.StreamHandler()
     stream_handler.setLevel(log_level[level])
-
     formatter = colorlog.ColoredFormatter(
         "%(log_color)s[%(asctime)s - %(levelname)s/%(name)s]: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
@@ -42,13 +45,10 @@ def setup_logger(name: str, level: str = "warning") -> logging.Logger:
             "CRITICAL": "magenta",
         },
     )
-
     file_handler.setFormatter(
         logging.Formatter("[%(asctime)s - %(levelname)s/%(name)s]: %(message)s")
     )
     stream_handler.setFormatter(formatter)
-
-    logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
 
     return logger
