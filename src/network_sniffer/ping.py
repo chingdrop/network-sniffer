f9a9ec29184ed364d04c5a3cff884d1fb004b0fb
Craig Hurley <chingdorp@pm.me>
2025-03-30 10:10:56 -0400

    change variable name

--- src/network_sniffer/ping.py
+++ src/network_sniffer/ping.py
@@ -16,25 +16,25 @@ bca = BroadcastAdapter()
 def arp_ping(target: str) -> list[dict]:
     pkt = create_arp_pkt(target)
     ans, _ = bca.sendp(pkt, timeout=3, verbose=0)
-    return [{"MAC": rcv[Ether].dst, "IP": rcv[ARP].psrc} for _, rcv in ans]
+    return [{"MAC": recv[Ether].dst, "IP": recv[ARP].psrc} for _, recv in ans]
 
 
 def icmp_ping(target: str) -> list[dict]:
     pkt = create_icmp_pkt(target)
     ans, _ = bca.send(pkt, timeout=3, verbose=0)
-    return [{"MAC": rcv[Ether].dst, "IP": rcv[IP].dst} for _, rcv in ans]
+    return [{"MAC": recv[Ether].dst, "IP": recv[IP].dst} for _, recv in ans]
 
 
 def tcp_ping(target: str) -> list[dict]:
     pkt = create_tcp_pkt(target, dport=80, flags="S")
     ans, _ = bca.send(pkt, timeout=3, verbose=0)
-    return [{"MAC": rcv[Ether].dst, "IP": rcv[IP].dst} for _, rcv in ans]
+    return [{"MAC": recv[Ether].dst, "IP": recv[IP].dst} for _, recv in ans]
 
 
 def udp_ping(target: str) -> list[dict]:
     pkt = create_udp_pkt(target, dport=0)
     ans, _ = bca.send(pkt, timeout=3, verbose=0)
-    return [{"MAC": rcv[Ether].dst, "IP": rcv[IP].dst} for _, rcv in ans]
+    return [{"MAC": recv[Ether].dst, "IP": recv[IP].dst} for _, recv in ans]
 
 
 def ping_active_hosts(target: str) -> list[dict]:
