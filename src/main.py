4a8872302a50fd8e9fcb42e8354b73e3c3d4379e
Craig Hurley <chingdorp@pm.me>
2025-03-30 06:57:25 -0400

    remove multi processing module, refactor quick enumeration to use new libraries

--- src/main.py
+++ src/main.py
@@ -1,31 +1,33 @@
-from network_sniffer.commands.local_network import LocalNetwork
-from network_sniffer.commands.multi_proc import MultiProcTasks
-from network_sniffer.commands.pings import Pings
+from network_sniffer.local import get_network_ip
+from network_sniffer.ping import ping_active_hosts
+from network_sniffer.enums import NON_PRIVILEGED_LOW_PORT, BASIC_PROTOCOLS
+from network_sniffer.scan import ack_scan, xmas_scan, protocol_scan
 
 
-def quick_enumeration(iface):
-    lan = LocalNetwork().get_network_ip(iface)
-    live_hosts = Pings().arp_ping(str(lan))
-    mpt = MultiProcTasks()
+def quick_enumeration(iface: str):
+    low_port_range = [i for i in range(1, NON_PRIVILEGED_LOW_PORT)]
+    proto_range = [i for i in range(1, BASIC_PROTOCOLS)]
+    lan = get_network_ip(iface)
+    live_hosts = ping_active_hosts(str(lan))
 
-    results = mpt.start_basic_scans(live_hosts)
-
-    print()
-    for target in results:
-        print("-" * 65)
-        if (
-            target["UnfilteredPorts"]
-            or target["OpenPorts"]
-            or target["ListeningProtocols"]
-        ):
-            print(f'{target["IP"]} : {target["MAC"]} could be a potential target')
-            print(
-                f'{len(target["UnfilteredPorts"])} ports unfiltered by a firewall'
-            )
-            print(f'{len(target["OpenPorts"])} ports open')
-            print(f'{len(target["ListeningProtocols"])} protocols listening')
-        else:
-            print(f'{target["IP"]} : {target["MAC"]} is secure')
-            print("No firewall rules affecting this host")
-            print("No open ports")
-            print("No protocols listening")
+    host_results = []
+    vuln_hosts = []
+    for host in live_hosts:
+        ack_res = ack_scan(host["IP"], low_port_range)
+        xmas_res = xmas_scan(host["IP"], low_port_range)
+        proto_res = protocol_scan(host["IP"], proto_range)
+        host_res = {
+            "ip": host["IP"],
+            "mac": host["MAC"],
+            "unfiltered_ports": ack_res["unfiltered"],
+            "open_ports": xmas_res["open"],
+            "listening_protocols": proto_res,
+        }
+        host_results.append(host_res)
+        if any(ack_res["unfiltered"], xmas_res["open"], proto_res):
+            vuln_hosts.append(host_res)
+    
+    print(f"\nQuick Enumeration Results for Network: {lan}")
+    print(f"Total Hosts Found: {len(live_hosts)}")
+    print(f"Total Vulnerable Hosts: {len(vuln_hosts)}\n")
+    return host_results, vuln_hosts
