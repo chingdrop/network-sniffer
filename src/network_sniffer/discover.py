11ce6d41f5256aaeb1f49be961f8b4fcabf00016
Craig Hurley <chingdorp@pm.me>
2025-03-30 05:08:16 -0400

    refactor ping funcs to pings module

--- src/network_sniffer/discover.py
+++ src/network_sniffer/discover.py
@@ -1,44 +1,5 @@
-from scapy.all import sr, srp, ARP, Ether, IP, ICMP, TCP, UDP
-
 from network_sniffer.commands.local_network import LocalNetwork
-
-
-def arp_ping(target):
-    host_list = []
-    ans, unans = srp(
-        Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target), timeout=3, verbose=0
-    )
-    for s, r in ans:
-        host = {"MAC": r[Ether].dst, "IP": r[ARP].psrc}
-        host_list.append(host)
-    return host_list
-
-
-def icmp_ping(target):
-    host_list = []
-    ans, unans = sr(IP(dst=target) / ICMP(), timeout=3, verbose=0)
-    for s, r in ans:
-        host = {"MAC": r[Ether].dst, "IP": r[IP].dst}
-        host_list.append(host)
-    return host_list
-
-
-def tcp_ping(target):
-    host_list = []
-    ans, unans = sr(IP(dst=target) / TCP(dport=80, flags="S"), timeout=3, verbose=0)
-    for s, r in ans:
-        host = {"MAC": r[Ether].dst, "IP": r[IP].dst}
-        host_list.append(host)
-    return host_list
-
-
-def udp_ping(target):
-    host_list = []
-    ans, unans = sr(IP(dst=target) / UDP(dport=0), timeout=3, verbose=0)
-    for s, r in ans:
-        host = {"MAC": r[Ether].dst, "IP": r[IP].dst}
-        host_list.append(host)
-    return host_list
+from src.network_sniffer.pings import arp_ping, icmp_ping, tcp_ping, udp_ping
 
 
 def quick_discover(iface):
--- src/network_sniffer/pings.py
+++ src/network_sniffer/pings.py
@@ -1,9 +1,43 @@
 import socket
 from scapy.layers.l2 import ARP, Ether
+from scapy.layers.inet import IP
 from scapy.sendrecv import srp
 
+from src.network_sniffer.packet import (
+    BroadcastAdapter,
+    create_arp_pkt,
+    create_icmp_pkt,
+    create_tcp_pkt,
+    create_udp_pkt,
+)
 
-def arp_ping(target: str, verbose=True) -> list[dict]:
+
+bca = BroadcastAdapter()
+
+
+def arp_ping(target: str) -> list[dict]:
+    pkt = create_arp_pkt(target)
+    ans, _ = bca.send_eth(pkt, timeout=3, verbose=0)
+    return [{"MAC": rcv[Ether].dst, "IP": rcv[ARP].psrc} for _, rcv in ans]
+
+
+def icmp_ping(target: str) -> list[dict]:
+    pkt = create_icmp_pkt(target)
+    ans, _ = bca.send_ip(pkt, timeout=3, verbose=0)
+    return [{"MAC": rcv[Ether].dst, "IP": rcv[IP].dst} for _, rcv in ans]
+
+
+def tcp_ping(target: str) -> list[dict]:
+    pkt = create_tcp_pkt(target, dport=80, flags="S")
+    ans, _ = bca.send_ip(pkt, timeout=3, verbose=0)
+    return [{"MAC": rcv[Ether].dst, "IP": rcv[IP].dst} for _, rcv in ans]
+
+
+def udp_ping(target: str) -> list[dict]:
+    pkt = create_udp_pkt(target, dport=0)
+    ans, _ = bca.send_ip(pkt, timeout=3, verbose=0)
+    return [{"MAC": rcv[Ether].dst, "IP": rcv[IP].dst} for _, rcv in ans]
+
     host_list = []
     # This definitely could be done better, but I don't want to nest try/excepts
     pkt = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=target)
